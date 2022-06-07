208e5b158e7c4cc4ff26a0bd3083827fced8d2f5
AlexBotswana <alfacama@outlook.fr>
2022-06-07 22:16:31 +0200

    title

--- scrapbook/main.py
+++ scrapbook/main.py
@@ -4,20 +4,31 @@ import csv
 
 #lien du site à scraper
 url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-reponse = requests.get(url)
-page_accueil = reponse.content
+response = requests.get(url)
+home_page = response.content
 
 # affiche la page HTML
-print(page_accueil)
+print(home_page)
 
 # transforme (parse) le HTML en objet BeautifulSoup
-soup_page_accueil = BeautifulSoup(page_accueil, "html.parser")
+soup_home_page = BeautifulSoup(home_page, "html.parser")
 
 # récupération des informations suivantes : product_page_url, universal_ product_code (upc), title, price_including_tax, 
 # price_excluding_tax, number_available, product_description, category, review_rating, image_url
-product_page_url_bs = soup_page_accueil.find_all("article", class_="product_pod")
-product_page_url = []
-for Product_page in product_page_url_bs:
-	product_page_url.append(Product_page.string)
+product_title_bs = soup_home_page.find_all("title")
+product_title_list = []
+for Product_title in product_title_bs:
+	product_title_list.append(Product_title.string)
 
+product_descript_bs = soup_home_page.find_all("description")
+product_descript_list = []
+for Product_descript in product_descript_bs:
+	product_descript_list.append(Product_descript.string)
 
+#write in csv file
+en_tete = ['Titre', 'Description']
+with open('data.csv', 'w') as csv_file:
+	writer = csv.writer(csv_file, delimiter=',')
+	writer.writerow(en_tete)
+	for titre, descript in zip(product_title_list, product_descript_list):
+		writer.writerow([titre, descript])
