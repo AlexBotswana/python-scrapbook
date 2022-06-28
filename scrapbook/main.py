62477f61e45bab9c28cecaa26c7cc72b3bc52ec7
AlexBotswana <alfacama@outlook.fr>
2022-06-28 18:33:50 +0200

    Data_formatting in csv file

--- scrapbook/main.py
+++ scrapbook/main.py
@@ -1,83 +1,100 @@
-#from inspect import classify_class_attrs
+from re import split
+from jmespath import search
 import requests
 from bs4 import BeautifulSoup
 import csv
 
+
+mapping_number = {"Zero": 0, "One": 1, "Two": 2, "Three": 3, "Four" :4, "Five": 5}
+
+
+	
+
+def extract_book_data(book_url):
+
+	response = requests.get(book_url)
+	html_book_page = response.content
+
+	# transforme (parse) le HTML en objet BeautifulSoup
+	soup_book_page = BeautifulSoup(html_book_page, "html.parser")
+
+	# récupération des informations suivantes : product_page_url (product_url) ,  , , image_url
+	# title
+	title = soup_book_page.find("h1").get_text()
+	
+	# Data in table-striped : universal_ product_code (upc), price_including_tax, price_excluding_tax, number_available, 
+	table = soup_book_page.find("table", {"class": "table table-striped"})
+	for row in table.find_all("tr"):
+		th = row.find("th").get_text()
+		td = row.find("td").get_text()
+        # UPC
+		if "UPC" in th:
+			upc = td
+        # price without tax
+		if "Price (excl. tax)" in th:
+			price_excluding_tax = td
+        # price with tax
+		if "Price (incl. tax)" in th:
+			price_including_tax = td
+        # stock
+		if "Availability" in th:
+			stock = td
+	
+	# product_description
+	product_descpt_bs = soup_book_page.find_all("p")
+	product_descpt_list = []
+	for product_descpt in product_descpt_bs:
+		product_descpt_list.append(product_descpt.text)
+	
+	#review_rating
+	rating_bs = soup_book_page.find("p", {"class": "star-rating"})
+	rating = str(rating_bs).split("\n")[0]
+	rating = rating.replace('<p class="star-rating ', "").replace('">', "")
+	rating = mapping_number[rating]
+	
+	# category
+	product_category_bs = soup_book_page.find_all("a", href="../category/books/poetry_23/index.html")
+	product_category_list = []
+	for product_category in product_category_bs:
+		product_category_list.append(product_category.text)
+	
+	# image url
+	url_img_bs = soup_book_page.img["src"]
+	url_site_root = "http://books.toscrap.com/"
+	url_img = url_site_root + url_img_bs.replace("../", "")
+	
+	print(book_url)
+	print(upc)
+	print(price_excluding_tax)
+	print(price_including_tax)
+	print(stock)
+	print(title)
+	print(product_descpt_list[3])
+	print(product_category_list[0])
+	print(rating)
+	print(url_img)
+
+
+
+	#write in csv file
+	# universal_ product_code (upc) = product_data_td_list[0], 
+	# price_including_tax = product_data_td_list[3], 
+	# price_excluding_tax = product_data_td_list [2], 
+	# number_available = product_data_td_list[5], 
+
+	en_tete = ['product_page_url','universal_ product_code (upc)','title','price_including_tax','price_excluding_tax', 'number_available', 'product description', 'category', 'review_rating', 'image_url']
+	with open('../scrapbook/data.csv', 'w') as csv_file:
+		writer = csv.writer(csv_file, delimiter=',')
+		writer.writerow(en_tete)
+		writer.writerow([book_url, upc, title, price_including_tax, price_excluding_tax, stock, product_descpt_list[3], product_category_list[0], rating, url_img])
+
+
+
 #lien du site à scraper
-product_url = "http://books.toscrape.com/catalogue/the-black-maria_991/index.html"
-response = requests.get(product_url)
-home_product_page = response.content
+responde_category = requests.get("http://books.toscrape.com/catalogue/category/books_1/index.html")
+home_category_page = responde_category.content
+soup_home_category_page = BeautifulSoup(home_category_page, "html.parser")
 
-# affiche la page HTML
-#print(home_page)
-
-# transforme (parse) le HTML en objet BeautifulSoup
-soup_home_product_page = BeautifulSoup(home_product_page, "html.parser")
-
-# récupération des informations suivantes : product_page_url (product_url) ,  , , image_url
-
-# Data in <td> : universal_ product_code (upc), price_including_tax, price_excluding_tax, number_available, 
-product_data_td_bs = soup_home_product_page.find_all("td")
-
-# title
-product_title_bs = soup_home_product_page.find_all("h1")
-
-# product_description
-product_descpt_bs = soup_home_product_page.find_all("p")
-
-# category
-product_category_bs = soup_home_product_page.find_all("a", href="../category/books/poetry_23/index.html")
-
-#review_rating
-#product_review_rating_bs = soup_home_product_page.find_all("p", class_="star-rating Three")
-
-# image url
-product_url_img_bs = soup_home_product_page.find_all("img")
-
-product_title_list = []
-for product_title in product_title_bs:
-	product_title_list.append(product_title.string)
-
-product_data_td_list = []
-for product_data_td in product_data_td_bs:
-	product_data_td_list.append(product_data_td)
-
-product_descpt_list = []
-for product_descpt in product_descpt_bs:
-	product_descpt_list.append(product_descpt)
-
-product_category_list = []
-for product_category in product_category_bs:
-	product_category_list.append(product_category)
-
-#product_review_rating_list = []
-#for product_review_rating in product_review_rating_bs:
-#	product_review_rating_list.append(product_review_rating)
-
-product_url_img_list = []
-for product_url_img in product_url_img_bs:
-	product_url_img_list.append(product_url_img)
-
-print(product_data_td_list)
-
-print(product_title_list[0])
-print(product_descpt_list[3])
-
-print(product_category_list[0])
-#print(product_review_rating_list)
-print(product_url_img_list[0])
-
-
-
-#write in csv file
-# universal_ product_code (upc) = product_data_td_list[0], 
-# price_including_tax = product_data_td_list[3], 
-# price_excluding_tax = product_data_td_list [2], 
-# number_available = product_data_td_list[5], 
-# review_rating = product_data_td_list [6] 
-
-en_tete = ['product_page_url','universal_ product_code (upc)','title','price_including_tax','price_excluding_tax', 'number_available', 'product description', 'category', 'review_rating', 'image_url']
-with open('../scrapbook/data.csv', 'w') as csv_file:
-	writer = csv.writer(csv_file, delimiter=',')
-	writer.writerow(en_tete)
-	writer.writerow([product_url, product_data_td_list[0], product_title_list[0], product_data_td_list[3], product_data_td_list[2], product_data_td_list[5], product_descpt_list[3], product_category_list, product_data_td_list[6], product_url_img_list])
+#product_url = "http://books.toscrape.com/catalogue/the-black-maria_991/index.html"
+book_url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+extract_book_data(book_url)
